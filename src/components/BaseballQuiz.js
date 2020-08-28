923b6a106de3ca92ac2fa5dc59e8be40cb7597ff
Mark <m.hern1415@gmail.com>
2020-08-27 17:20:31 -0700

    added baseball quiz

---
+++ src/components/BaseballQuiz.js
@@ -0,0 +1,136 @@
+import React from "react";
+import { BaseballData } from "./BaseballData";
+import { Link } from 'react-router-dom';
+
+
+class BaseballQuiz extends React.Component {
+  state = {
+    currentQuestion: 0,
+    myAnswer: null,
+    options: [],
+    score: 0,
+    disabled: true,
+    isEnd: false
+  };
+  refreshPage = () => {
+    window.location.reload()
+  }
+  loadQuizData = () => {
+    // console.log(quizData[0].question)
+    this.setState(() => {
+      return {
+        questions: BaseballData[this.state.currentQuestion].question,
+        answer: BaseballData[this.state.currentQuestion].answer,
+        options: BaseballData[this.state.currentQuestion].options
+      };
+    });
+  };
+
+  componentDidMount() {
+    this.loadQuizData();
+  }
+  nextQuestionHandler = () => {
+    // console.log('test')
+    const { myAnswer, answer, score } = this.state;
+
+    if (myAnswer === answer) {
+      this.setState({
+        score: score + 1
+      });
+    }
+   
+    this.setState({
+      currentQuestion: this.state.currentQuestion + 1
+    });
+    console.log(this.state.currentQuestion);
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.currentQuestion !== prevState.currentQuestion) {
+      this.setState(() => {
+        return {
+          disabled: true,
+          questions: BaseballData[this.state.currentQuestion].question,
+          options: BaseballData[this.state.currentQuestion].options,
+          answer: BaseballData[this.state.currentQuestion].answer
+        };
+      });
+    }
+  }
+  //check answer
+  checkAnswer = answer => {
+    this.setState({ myAnswer: answer, disabled: false });
+  };
+  finishHandler = () => {
+    if (this.state.currentQuestion === BaseballData.length - 1) {
+      this.setState({
+        isEnd: true
+      });
+    }
+    if (this.state.myAnswer === this.state.answer) {
+      this.setState({
+        score: this.state.score + 1
+      });
+    }
+  };
+  render() {
+    const { options, myAnswer, currentQuestion, isEnd } = this.state;
+
+    if (isEnd) {
+      return (
+        <div className="result">
+          <h3>Game Over your Final score is {this.state.score} points </h3>
+          <div>
+            The correct answer's were:
+
+
+            <ul>
+              {BaseballData.map((item, index) => (
+                <li className="ui floating message options" key={index}>
+                  {item.answer}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <button type="button" onClick={ this.refreshPage }> <span>Try Again!</span> </button> 
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <h1>{this.state.questions} </h1>
+          <span>{`Questions ${currentQuestion}  out of ${BaseballData.length -
+            1} remaining `}</span>
+          {options.map(option => (
+            <p
+              key={option.id}
+              className={`ui floating message options
+         ${myAnswer === option ? "selected" : null}
+         `}
+              onClick={() => this.checkAnswer(option)}
+            >
+              {option}
+            </p>
+          ))}
+          {currentQuestion < BaseballData.length - 1 && (
+            <button
+              className="ui inverted button"
+              disabled={this.state.disabled}
+              onClick={this.nextQuestionHandler}
+            >
+              Next
+            </button>
+          )}
+          {/* //adding a finish button */}
+          {currentQuestion === BaseballData.length - 1 && (
+            <button className="ui inverted button" onClick={this.finishHandler}>
+              Finish
+            </button>
+          )}
+        </div>
+      );
+    }
+  }
+}
+
+export default BaseballQuiz;
