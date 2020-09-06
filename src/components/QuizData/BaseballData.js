ca0c3f5d7c7ff40ef84d2ec7be27956d8d6c6dc8
Mark <m.hern1415@gmail.com>
2020-09-05 22:21:31 -0700

    created new folders

---
+++ src/components/QuizData/BaseballData.js
@@ -0,0 +1,27 @@
+export const BaseballData = [
+    {
+      id: 0,
+      question: ` What is the name of a contest where baseball players try to hit as many home runs as they can?`,
+      options: [`The Home Run Tournament`, `The Home Run Derby`, `The Home Run Competition`, `The Home Run Contest`],
+      answer: `The Home Run Derby`
+    },
+    {
+      id: 1,
+      question: `Which former player has the nickname "The Say Hey Kid"?`,
+      options: [`Babe Ruth`, `Hank Aaron`, `Willie Mays`, `Alex Rodriguez`],
+      answer: `Willie Mays`
+    },
+    {
+      id: 3,
+      question: `Which team represented the National League in the World Series in 2010, 2012, and 2014?`,
+      options: [`San Francisco Giants`, `Los Angeles Dodgers`, `Philadelphia Phillies`, `The New York Yankees`],
+      answer: `San Francisco Giants`
+    },
+    {
+      id: 4,
+      question: `During which inning is "Take Me Out to the Ballgame" traditionally sung?`,
+      options: [`8th`, `6th`, `9th`, `7th`],
+      answer: `7th`
+    }
+  ];
+  
