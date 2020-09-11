b99d8c7daab61f37a745431eefe367190651a07c
Mark <m.hern1415@gmail.com>
2020-09-10 21:58:05 -0700

    added football questions

--- src/components/QuizData/FootballData.js
+++ src/components/QuizData/FootballData.js
@@ -1,27 +1,27 @@
 export const FootballData = [
     {
       id: 0,
-      question: ` What is the name of a contest where baseball players try to hit as many home runs as they can?`,
-      options: [`The Home Run Tournament`, `The Home Run Derby`, `The Home Run Competition`, `The Home Run Contest`],
-      answer: `The Home Run Derby`
+      question: ` Who holds the record for career receptions?`,
+      options: [`Larry Fitzgerald`, `Tony Gonzalez`, `Jerry Rice`, `Randy Moss`],
+      answer: `Jerry Rice`
     },
     {
       id: 1,
-      question: `Which former player has the nickname "The Say Hey Kid"?`,
-      options: [`Babe Ruth`, `Hank Aaron`, `Willie Mays`, `Alex Rodriguez`],
-      answer: `Willie Mays`
+      question: `This NFL team came back from a 25 point deficit to win the Super Bowl. It was the largest comeback in Super Bowl history.`,
+      options: [`New England Patriots`, `Denver Broncos`, `Indianapolis Colts`, `Kansas City Chiefs`],
+      answer: `New England Patriots`
+    },
+    {
+      id: 2,
+      question: `The Patriots and Steelers lead NFL franchises with 6 Super Bowl Championships. Which two franchises are tied for second with 5 Super Bowl Championships?`,
+      options: [`49ers and Broncos`, `49ers and Cowboys`, `Cowboys and Packers`, `Packers and 49ers`],
+      answer: `49ers and Cowboys`
     },
     {
       id: 3,
-      question: `Which team represented the National League in the World Series in 2010, 2012, and 2014?`,
-      options: [`San Francisco Giants`, `Los Angeles Dodgers`, `Philadelphia Phillies`, `The New York Yankees`],
-      answer: `San Francisco Giants`
-    },
-    {
-      id: 4,
-      question: `During which inning is "Take Me Out to the Ballgame" traditionally sung?`,
-      options: [`8th`, `6th`, `9th`, `7th`],
-      answer: `7th`
+      question: `What is the name of the NFL's all-star exhibition game??`,
+      options: [`The NFL Classis`, `League All-Star Game`, `The NFL's Best Game`, `Pro Bowl`],
+      answer: `Pro Bowl`
     }
   ];
   
