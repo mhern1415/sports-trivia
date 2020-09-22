52e624b28579972f4ff03aca6644bf8def89e43f
Mark <m.hern1415@gmail.com>
2020-09-21 21:49:53 -0700

    changed difficulty

--- src/components/QuizData/Baseball/BaseballData.js
+++ src/components/QuizData/Baseball/BaseballData.js
@@ -1,9 +1,9 @@
 export const BaseballData = [
     {
       id: 0,
-      question: ` What is the name of a contest where baseball players try to hit as many home runs as they can?`,
-      options: [`The Home Run Tournament`, `The Home Run Derby`, `The Home Run Competition`, `The Home Run Contest`],
-      answer: `The Home Run Derby`
+      question: `Which team moved to D.C. to become the Washington Nationals in 2005?`,
+      options: [`Kansas City Monarchs`, `Los Angeles Sparks`, `Montreal Expos`, `Houston Oilers`],
+      answer: `Montreal Expos`
     },
     {
       id: 1,
@@ -19,9 +19,33 @@ export const BaseballData = [
     },
     {
       id: 3,
-      question: `During which inning is "Take Me Out to the Ballgame" traditionally sung?`,
-      options: [`8th`, `6th`, `9th`, `7th`],
-      answer: `7th`
+      question: `Which city is home to MLB's oldest stadium?`,
+      options: [`Chicago`, `Los Angeles`, `New York`, `Boston`],
+      answer: `Boston`
+    },
+    {
+      id: 4,
+      question: `What country has contributed the most players to Major League Baseball outside of the United States?`,
+      options: [`Japan`, `Dominican Republic`, `Cuba`, `Venezuela`],
+      answer: `Dominican Republic`
+    },
+    {
+      id: 5,
+      question: `When the Chicago Cubs won the World Series in 2016, which curse was said to be broken?`,
+      options: [`The Curse of Bartman`, `The Curse of Coogan's Bluff`, `The Curse of the Billy Goat`, `The Curse of the Bambino`],
+      answer: `The Curse of the Billy Goat`
+    },
+    {
+      id: 6,
+      question: `1998 saw a race between the St. Louis Cardinals' Mark McGwire and the Chicago Cubs' Sammy Sosa to break which player's 61 home run record?`,
+      options: [`Babe Ruth`, `Roger Maris`, `Hank Aaron`, `Barry Bonds`],
+      answer: `Roger Maris`
+    },
+    {
+      id: 7,
+      question: `Where is the National Baseball Hall of Fame & Museum located?`,
+      options: [`Cooperstown, NY`, `Memphis, TN`, `Cleveland, OH`, `Anaheim, CA`],
+      answer: `Cooperstown, NY`
     }
   ];
   
