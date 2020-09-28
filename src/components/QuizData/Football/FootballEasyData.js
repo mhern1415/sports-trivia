1b0f633cb77d09aca280af5bea6c0bea905fbd23
Mark <m.hern1415@gmail.com>
2020-09-27 22:12:38 -0700

    added questions

--- src/components/QuizData/Football/FootballEasyData.js
+++ src/components/QuizData/Football/FootballEasyData.js
@@ -1,45 +1,45 @@
 export const FootballEasyData = [
     {
       id: 0,
-      question: `How football many total bases are there on a baseball field?`,
-      options: [`2`, `3`, `4`, `5`],
-      answer: `4`
+      question: `In the NFL, how many points is a touchdown worth?`,
+      options: [`3`, `5`, `6`, `7`],
+      answer: `6`
     },
     {
       id: 1,
-      question: `When a player is safe at home plate, what did he just score?`,
-      options: [`A goal`, `A field goal`, `A touchdown`, `A run`, `A score`],
-      answer: `A run`
+      question: `Which position is responsible for calling the play on the field?`,
+      options: [`Tight End`, `Quarterback`, `Center`, `Linebacker`, `Wide Receiver`],
+      answer: `Quarterback`
     },
     {
       id: 2,
-      question: `The Yankees represent which American city?`,
-      options: [`San Francisco`, `Los Angeles`, `Philadelphia`, `New York`, `Boston`, `Detroit`],
-      answer: `New York`
+      question: `How many players take the field on defense?`,
+      options: [`9`, `13`, `12`, `11`, `14`],
+      answer: `11`
     },
     {
       id: 3,
-      question: `How many outs is each teach allowed to commit before the teams switch sides?`,
-      options: [`3`, `4`, `2`, `5`],
-      answer: `3`
+      question: `What is it called when the quarterback gets tackled before throwing the football?`,
+      options: [`Sack`, `Inside Tackle`, `Scrimmage Tackle`, `Down `],
+      answer: `Sack`
     },
     {
       id: 4,
-      question: `A starting baseball lineup is composed of how many players?`,
-      options: [`7`, `8`, `9`, `10`],
-      answer: `9`
+      question: `What is the name of the line where the ball is snapped?`,
+      options: [`Beginning Line`, `Line of Scrimmage`, `Line of Play`, `Starting Line`],
+      answer: `Line of Scrimmage`
     },
     {
       id: 5,
-      question: `When a player commits an error, what did he do?`,
-      options: [`Getting out on the basebaths`, `Making a mistake on defense`, `Striking out`, `Forgetting to wear a cup`],
-      answer: `Making a mistake on defense`
+      question: `How long is the football field, not including the end zones?`,
+      options: [`100 Yards`, `110 Yards`, `120 Yards`, `90 Yards`],
+      answer: `100 Yards`
     },
     {
       id: 6,
-      question: `In baseball, what is a southpaw?`,
-      options: [`A player from Alabama`, `A right handed batter`, `A switch hitting batter`, `A left handed pitcher`],
-      answer: `A left handed pitcher`
+      question: `IWhat is used to determine who gets the football first to start a game?`,
+      options: [`Rock, Paper, Scissors`, `Home team gets the ball first`, `Away team gets the ball first`, `Coin Flip`],
+      answer: `Coin Flip`
     },
     {
       id: 7,
