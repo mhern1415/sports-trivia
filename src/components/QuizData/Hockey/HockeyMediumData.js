d4b5271d2b7d8391830c1dadb647733ec2839af2
Mark <m.hern1415@gmail.com>
2020-10-03 22:23:05 -0700

    added hockey medium

---
+++ src/components/QuizData/Hockey/HockeyMediumData.js
@@ -0,0 +1,53 @@
+export const FootballMediumData = [
+    {
+      id: 0,
+      question: `Peyton Manning is the only starting quarterback to win Super Bowls with two different teams. Name both teams.`,
+      options: [`Colts and Falcons`, `Broncos and Panthers`, `Colts and Broncos`, `Patriots and Colts`],
+      answer: `Colts and Broncos`
+    },
+    {
+      id: 1,
+      question: `Which one of these teams has never won a Super Bowl?`,
+      options: [`Minnesota Vikings`, `Arizona Cardinals`, `Los Angeles Chargers`, `Houston Texans`, `All of the above`],
+      answer: `All of the above`
+    },
+    {
+      id: 2,
+      question: ` Who holds the record for career receptions?`,
+      options: [`Larry Fitzgerald`, `Tony Gonzalez`, `Jerry Rice`, `Randy Moss`],
+      answer: `Jerry Rice`
+    },
+    {
+      id: 3,
+      question: `The Patriots and Steelers lead NFL franchises with 6 Super Bowl Championships. Which two franchises are tied for second with 5 Super Bowl Championships?`,
+      options: [`49ers and Broncos`, `49ers and Cowboys`, `Cowboys and Packers`, `Packers and 49ers`],
+      answer: `49ers and Cowboys`
+    },
+    {
+      id: 4,
+      question: `What is the name of the NFL's all-star exhibition game?`,
+      options: [`Pro Bowl`, `League All-Star Game`, `The NFL's Best Game`, `The NFL Classic`],
+      answer: `Pro Bowl`
+    },
+    {
+      id: 5,
+      question: `This NFL team came back from a 25 point deficit to win the Super Bowl. It was the largest comeback in Super Bowl history.`,
+      options: [`Kansas City Chiefs`, `Denver Broncos`, `Indianapolis Colts`, `New England Patriots`],
+      answer: `New England Patriots`
+    },
+    {
+      id: 6,
+      question: `Which NFL Quarterback has been in the most Super Bowls?`,
+      options: [`Tom Brady`, `Joe Montana`, `John Elway`, `Troy Aikman`],
+      answer: `Tom Brady`
+    },
+    {
+      id: 7,
+      question: `This NFL player once celebrated a touchdown by grabbing a sharpie from his sock and signing the ball that he gave to a spectator.`,
+      options: [`Randy Moss`, `Terrell Owens`, `Jerry Rice`, `Larry Fitzgerald`, `Marvin Harrison`],
+      answer: `Terrell Owens`
+    }
+
+
+  ];
+  
