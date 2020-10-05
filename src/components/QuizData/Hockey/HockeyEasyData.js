1679b07bfb3bb840db26eb6a7274d285478fea29
Mark <m.hern1415@gmail.com>
2020-10-04 21:58:29 -0700

    added hard quiz and medium for hockey

---
+++ src/components/QuizData/Hockey/HockeyEasyData.js
@@ -0,0 +1,53 @@
+export const FootballEasyData = [
+    {
+      id: 0,
+      question: `In the NFL, how many points is a touchdown worth?`,
+      options: [`3`, `5`, `6`, `7`],
+      answer: `6`
+    },
+    {
+      id: 1,
+      question: `Which position is responsible for calling the plays on the field?`,
+      options: [`Tight End`, `Quarterback`, `Center`, `Linebacker`, `Wide Receiver`],
+      answer: `Quarterback`
+    },
+    {
+      id: 2,
+      question: `How many players take the field on defense?`,
+      options: [`9`, `13`, `12`, `11`, `14`],
+      answer: `11`
+    },
+    {
+      id: 3,
+      question: `What is it called when the quarterback gets tackled before throwing the football?`,
+      options: [`Sack`, `Inside Tackle`, `Scrimmage Tackle`, `Down `],
+      answer: `Sack`
+    },
+    {
+      id: 4,
+      question: `What is the name of the line where the ball is snapped?`,
+      options: [`Beginning Line`, `Line of Scrimmage`, `Line of Play`, `Starting Line`],
+      answer: `Line of Scrimmage`
+    },
+    {
+      id: 5,
+      question: `How long is the football field, not including the end zones?`,
+      options: [`100 Yards`, `110 Yards`, `120 Yards`, `90 Yards`],
+      answer: `100 Yards`
+    },
+    {
+      id: 6,
+      question: `What is used to determine who gets the football first to start a game?`,
+      options: [`Rock, Paper, Scissors`, `Home team gets the ball first`, `Away team gets the ball first`, `Coin Flip`],
+      answer: `Coin Flip`
+    },
+    {
+      id: 7,
+      question: `How many points is a field goal worth?`,
+      options: [`4`, `3`, `5`, `2`],
+      answer: `3`
+    }
+
+
+  ];
+  
