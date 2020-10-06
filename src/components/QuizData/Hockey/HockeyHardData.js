7dceee86fdc33cf792ec7ce5f8e412a1dbe03a74
Mark <m.hern1415@gmail.com>
2020-10-05 20:35:32 -0700

    added data to hard hockey

---
+++ src/components/QuizData/Hockey/HockeyHardData.js
@@ -0,0 +1,53 @@
+export const FootballData = [
+    {
+      id: 0,
+      question: `How many years must a player be retired to be eligible for the Pro Football Hall of Fame?`,
+      options: [`3 years`, `1 Year`, `4 Years`, `5 Years`],
+      answer: `5 Years`
+    },
+    {
+      id: 1,
+      question: `Which of the following teams were not part of the original NFL?`,
+      options: [`Chicago Bears`, `Miami Dolphins`, `Green Bay Packers`, `New York Giants`, `All of the above`],
+      answer: `Miami Dolphins`
+    },
+    {
+      id: 2,
+      question: `Which team was given the nickname "America's Team" in the 1979 NFL season?`,
+      options: [`Houston Texans`, `New England Patriots`, `Dallas Cowboys`, `Las Vegas Raiders`],
+      answer: `Dallas Cowboys`
+    },
+    {
+      id: 3,
+      question: `How many players are on each NFL team's roster?`,
+      options: [`53`, `45`, `48`, `59`],
+      answer: `53`
+    },
+    {
+      id: 4,
+      question: `Which team has the only undefeated season, including the postseason and the Super Bowl?`,
+      options: [`New England Patriots`, `Miami Dolphins`, `San Francisco 49ers`, `Dallas Cowboys`, `Green Bay Packers`],
+      answer: `Miami Dolphins`
+    },
+    {
+      id: 5,
+      question: `In every NFL game, how many referees are on the field officiating the game?`,
+      options: [`4`, `5`, `6`, `7`],
+      answer: `7`
+    },
+    {
+      id: 6,
+      question: `Which team holds the record of most points scored in a Super Bowl with 55?`,
+      options: [`New England Patriots`, `San Francisco 49ers`, `Dallas Cowboys`, `Las Vegas Raiders`, `Green Bay Packers`],
+      answer: `San Francisco 49ers`
+    },
+    {
+      id: 7,
+      question: `This kicker has the longest field goal in NFL history.`,
+      options: [`Matt Prater`, `Tom Dempsey`, `Sebastian Janikowsk`, `David Akers`],
+      answer: `Matt Prater`
+    }
+  
+  
+  ];
+  
