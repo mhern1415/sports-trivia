0182146cc04ac4ea1f5337f4287a43eecce4e1db
Mark <m.hern1415@gmail.com>
2020-10-08 09:34:42 -0700

    finished easy quiz data

--- src/components/QuizData/Hockey/HockeyEasyData.js
+++ src/components/QuizData/Hockey/HockeyEasyData.js
@@ -13,39 +13,39 @@ export const FootballEasyData = [
     },
     {
       id: 2,
-      question: `How many players take the field on defense?`,
-      options: [`9`, `13`, `12`, `11`, `14`],
-      answer: `11`
+      question: `What is the name of the championship trophy in the NHL?`,
+      options: [`The Stanley Cup`, `The NHL Cup`, `The President's Trophy`, `The World Hockey Cup`],
+      answer: `The Stanley Cup`
     },
     {
       id: 3,
-      question: `What is it called when the quarterback gets tackled before throwing the football?`,
-      options: [`Sack`, `Inside Tackle`, `Scrimmage Tackle`, `Down `],
-      answer: `Sack`
+      question: `The Stars play in which American City?`,
+      options: [`San Jose`, `New York`, `Los Angeles`, `Dallas`],
+      answer: `Dallas`
     },
     {
       id: 4,
-      question: `What is the name of the line where the ball is snapped?`,
-      options: [`Beginning Line`, `Line of Scrimmage`, `Line of Play`, `Starting Line`],
-      answer: `Line of Scrimmage`
+      question: `What does NHL stand for?`,
+      options: [`Northern Hockey League`, `National Hockey Link`, `Navy Hockey League`, `National Hockey League`],
+      answer: `National Hockey League`
     },
     {
       id: 5,
-      question: `How long is the football field, not including the end zones?`,
-      options: [`100 Yards`, `110 Yards`, `120 Yards`, `90 Yards`],
-      answer: `100 Yards`
+      question: `In hockey, what is a hat trick?`,
+      options: [`A team wins a game when the coach wears a lucky hat.`, `A player scoring 3 goals in a game`, `A player tricks the ref into reversing a call.`, `A goalie makes a save with his/her helmet.`],
+      answer: `A player scoring 3 goals in a game`
     },
     {
       id: 6,
-      question: `What is used to determine who gets the football first to start a game?`,
-      options: [`Rock, Paper, Scissors`, `Home team gets the ball first`, `Away team gets the ball first`, `Coin Flip`],
-      answer: `Coin Flip`
+      question: `Where does a player go if he recieves a penalty?`,
+      options: [`Penalty Box`, `Locker Room`, `He is disqualified`, `The bench`],
+      answer: `Penalty Box`
     },
     {
       id: 7,
-      question: `How many points is a field goal worth?`,
-      options: [`4`, `3`, `5`, `2`],
-      answer: `3`
+      question: `What does the "five-hole" in hockey refer to??`,
+      options: [`The place where hockey pucks are stored before a game.`, `The place where a player sits while serving a penalty.`, `The spot on the ice where the puck is dropped.`, `The space between a goalie's leg pads.`],
+      answer: `The space between a goalie's leg pads.`
     }
 
 
