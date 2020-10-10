7367d6eb28b1eb54a48ede3ac9be58e11f4b5885
Mark <m.hern1415@gmail.com>
2020-10-09 22:20:49 -0700

    hockey medium

--- src/components/QuizData/Hockey/HockeyMediumData.js
+++ src/components/QuizData/Hockey/HockeyMediumData.js
@@ -1,53 +1,52 @@
-export const FootballMediumData = [
-    {
-      id: 0,
-      question: `Peyton Manning is the only starting quarterback to win Super Bowls with two different teams. Name both teams.`,
-      options: [`Colts and Falcons`, `Broncos and Panthers`, `Colts and Broncos`, `Patriots and Colts`],
-      answer: `Colts and Broncos`
-    },
-    {
-      id: 1,
-      question: `Which one of these teams has never won a Super Bowl?`,
-      options: [`Minnesota Vikings`, `Arizona Cardinals`, `Los Angeles Chargers`, `Houston Texans`, `All of the above`],
-      answer: `All of the above`
-    },
-    {
-      id: 2,
-      question: ` Who holds the record for career receptions?`,
-      options: [`Larry Fitzgerald`, `Tony Gonzalez`, `Jerry Rice`, `Randy Moss`],
-      answer: `Jerry Rice`
-    },
-    {
-      id: 3,
-      question: `The Patriots and Steelers lead NFL franchises with 6 Super Bowl Championships. Which two franchises are tied for second with 5 Super Bowl Championships?`,
-      options: [`49ers and Broncos`, `49ers and Cowboys`, `Cowboys and Packers`, `Packers and 49ers`],
-      answer: `49ers and Cowboys`
-    },
-    {
-      id: 4,
-      question: `What is the name of the NFL's all-star exhibition game?`,
-      options: [`Pro Bowl`, `League All-Star Game`, `The NFL's Best Game`, `The NFL Classic`],
-      answer: `Pro Bowl`
-    },
-    {
-      id: 5,
-      question: `This NFL team came back from a 25 point deficit to win the Super Bowl. It was the largest comeback in Super Bowl history.`,
-      options: [`Kansas City Chiefs`, `Denver Broncos`, `Indianapolis Colts`, `New England Patriots`],
-      answer: `New England Patriots`
-    },
-    {
-      id: 6,
-      question: `Which NFL Quarterback has been in the most Super Bowls?`,
-      options: [`Tom Brady`, `Joe Montana`, `John Elway`, `Troy Aikman`],
-      answer: `Tom Brady`
-    },
-    {
-      id: 7,
-      question: `This NFL player once celebrated a touchdown by grabbing a sharpie from his sock and signing the ball that he gave to a spectator.`,
-      options: [`Randy Moss`, `Terrell Owens`, `Jerry Rice`, `Larry Fitzgerald`, `Marvin Harrison`],
-      answer: `Terrell Owens`
-    }
+export const FootballEasyData = [
+  {
+    id: 0,
+    question: `What hockey position player is most likely to wear a throat protector?`,
+    options: [`Forward`, `Center`, `Goalie`, `Defenseman`],
+    answer: `Goalie`
+  },
+  {
+    id: 1,
+    question: `How many NHL teams are in California?`,
+    options: [`2`, `3`, `1`, `0`, `4`],
+    answer: `3`
+  },
+  {
+    id: 2,
+    question: `What is the name of the championship trophy in the NHL?`,
+    options: [`The Stanley Cup`, `The NHL Cup`, `The President's Trophy`, `The World Hockey Cup`],
+    answer: `The Stanley Cup`
+  },
+  {
+    id: 3,
+    question: `The Stars play in which American City?`,
+    options: [`San Jose`, `New York`, `Los Angeles`, `Dallas`],
+    answer: `Dallas`
+  },
+  {
+    id: 4,
+    question: `What does NHL stand for?`,
+    options: [`Northern Hockey League`, `National Hockey Link`, `Navy Hockey League`, `National Hockey League`],
+    answer: `National Hockey League`
+  },
+  {
+    id: 5,
+    question: `In hockey, what is a hat trick?`,
+    options: [`A team wins a game when the coach wears a lucky hat.`, `A player scoring 3 goals in a game`, `A player tricks the ref into reversing a call.`, `A goalie makes a save with his/her helmet.`],
+    answer: `A player scoring 3 goals in a game`
+  },
+  {
+    id: 6,
+    question: `Where does a player go if he recieves a penalty?`,
+    options: [`Penalty Box`, `Locker Room`, `He is disqualified`, `The bench`],
+    answer: `Penalty Box`
+  },
+  {
+    id: 7,
+    question: `What does the "five-hole" in hockey refer to??`,
+    options: [`The place where hockey pucks are stored before a game.`, `The place where a player sits while serving a penalty.`, `The spot on the ice where the puck is dropped.`, `The space between a goalie's leg pads.`],
+    answer: `The space between a goalie's leg pads.`
+  }
 
 
-  ];
-  
+];
