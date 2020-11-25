aa30f5f5e838aad0ed60309926ba47f1e86cf834
Mark <m.hern1415@gmail.com>
2020-11-24 22:47:46 -0800

    added folder for soccer

---
+++ src/components/QuizData/Soccer/SoccerEasyData.js
@@ -0,0 +1,53 @@
+export const HockeyEasyData = [
+    {
+      id: 0,
+      question: `What hockey position player is most likely to wear a throat protector?`,
+      options: [`Forward`, `Center`, `Goalie`, `Defenseman`],
+      answer: `Goalie`
+    },
+    {
+      id: 1,
+      question: `How many NHL teams are in California?`,
+      options: [`2`, `3`, `1`, `0`, `4`],
+      answer: `3`
+    },
+    {
+      id: 2,
+      question: `What is the name of the championship trophy in the NHL?`,
+      options: [`The Stanley Cup`, `The NHL Cup`, `The President's Trophy`, `The World Hockey Cup`],
+      answer: `The Stanley Cup`
+    },
+    {
+      id: 3,
+      question: `The Stars play in which American City?`,
+      options: [`San Jose`, `New York`, `Los Angeles`, `Dallas`],
+      answer: `Dallas`
+    },
+    {
+      id: 4,
+      question: `What does NHL stand for?`,
+      options: [`Northern Hockey League`, `National Hockey Link`, `Navy Hockey League`, `National Hockey League`],
+      answer: `National Hockey League`
+    },
+    {
+      id: 5,
+      question: `In hockey, what is a hat trick?`,
+      options: [`A team wins a game when the coach wears a lucky hat.`, `A player scoring 3 goals in a game`, `A player tricks the ref into reversing a call.`, `A goalie makes a save with his/her helmet.`],
+      answer: `A player scoring 3 goals in a game`
+    },
+    {
+      id: 6,
+      question: `Where does a player go if he recieves a penalty?`,
+      options: [`Penalty Box`, `Locker Room`, `He is disqualified`, `The bench`],
+      answer: `Penalty Box`
+    },
+    {
+      id: 7,
+      question: `What does the "five-hole" in hockey refer to?`,
+      options: [`The place where hockey pucks are stored before a game.`, `The place where a player sits while serving a penalty.`, `The spot on the ice where the puck is dropped.`, `The space between a goalie's leg pads.`],
+      answer: `The space between a goalie's leg pads.`
+    }
+
+
+  ];
+  
