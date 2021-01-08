aeff4755e225085343114488f5713a1aea116f68
Mark <m.hern1415@gmail.com>
2021-01-07 21:44:36 -0800

    added dummy data for hard

---
+++ src/components/QuizData/Soccer/SoccerHardData.js
@@ -0,0 +1,53 @@
+export const SoccerHardData = [
+    {
+      id: 0,
+      question: `How long is a goalie allowed to have the ball in his/her hands?`,
+      options: [`3 Seconds`, `5 Seconds`, `As long as necessary`, `6 Seconds`],
+      answer: `6 Seconds`
+    },
+    {
+      id: 1,
+      question: `"Striker" is another name for which position?`,
+      options: [`Forward`, `Midfielder`, `Goalie`, `Football`, `Team Captain`],
+      answer: `Forward`
+    },
+    {
+      id: 2,
+      question: `Which of these is NOT a real soccer position?`,
+      options: [`Striker`, `Midfielder`, `Wingman`, `Fullback`],
+      answer: `Wingman`
+    },
+    {
+      id: 3,
+      question: `If an offensive player is fouled in the box, the result is a _________kick. `,
+      options: [`Corner`, `Direct`, `Penalty`, `Indirect`],
+      answer: `Penalty`
+      },
+    {
+      id: 4,
+      question: `A penalty kick is taken _____________ yards away from the goal line`,
+      options: [`5 Yards`, `12 Yards`, `27 Yards`, `50 Yards`],
+      answer: `12 Yards`
+    },
+    {
+      id: 5,
+      question: `What basic soccer skill is used when the ball goes out of bounds on the sideline?`,
+      options: [`Throw In`, `Bicycle Kick`, `Dribbling`, `One-Time Pass`],
+      answer: `Throw In`
+    },
+    {
+      id: 6,
+      question: `If the defending team kicks the ball out on their own goal line, what kind of kick brings the ball back in?`,
+      options: [`Penalty Kick`, `Goal Kick`, `Corner Kick`, `Free Kick`],
+      answer: `Corner Kick`
+    },
+    {
+      id: 7,
+      question: `Which of the following pieces of equipment is not allowed for any player?`,
+      options: [`Tape on socks`, `Facemask`, `Hijab`, `A shirt without sleeves`],
+      answer: `A shirt without sleeves`
+    }
+
+
+  ];
+  
